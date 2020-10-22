ed528d30770d5ef088e017ef28494679666752fc
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-10-22 16:13:55 +0300

    ошибка

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+// import toggleTheme from './js/theme.js';
+
+// input.addEventListener('click', toggleTheme);
---
+++ src/js/theme.js
@@ -0,0 +1,19 @@
+// const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+// const input = document.querySelector('#theme-switch-toggle');
+// const body = document.querySelector('body');
+
+// body.classList.add('light-theme');
+
+// const toggleTheme = function() {
+//     if (body.classList.contains('light-theme')) {
+//         body.classList.remove('light-theme');
+//         body.classList.add('dark-theme');
+//     } 
+// };
+
+// export default toggleTheme;
+
