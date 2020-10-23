e5a8ab10d6bc8ee24994fdf8b8682c6409f85738
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-10-23 14:43:40 +0300

    Шаблонизация

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './styles.css';
 import toggleTheme from './js/theme.js';
+import makeMenu from './templates/menu.handlebars';
+import menu from './menu.json';
 
 
 const input = document.querySelector('#theme-switch-toggle');
 
 input.addEventListener('click', toggleTheme);
+
+document.querySelector('.js-menu').innerHTML = makeMenu(menu);
